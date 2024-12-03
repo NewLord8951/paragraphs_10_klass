92df61d299678d8e284237c47c11d4464929db2d
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-03 12:07:40 +0000

    63.2

---
+++ task63.2.py
@@ -0,0 +1,7 @@
+n = int(input('Введите количество элементов в массиве: '))
+a = []
+for i in range(n):
+    a.append(int(input()))
+max_elem = max(a)
+count_max_elem = a.count(max_elem)
+print(f'Количество максимальных элементов в массиве: {count_max_elem}')

18b6f7c60740cf01deb530da5781077af4c4f7db
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-05 09:45:35 +0000

    64.1

---
+++ task64.1.py
@@ -0,0 +1,11 @@
+from collections import Counter, randint
+
+print("Массив:")
+a = [randint(0, 10) for i in range(10)]
+print(a)
+
+print("После сортировки:")
+a = sorted(a)
+c = Counter(a)
+print(a)
+print(c)

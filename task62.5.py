91839047d0e861d30688d61247650044e58706d0
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-05 10:26:18 +0000

    62.5

--- task62.5.py
+++ task62.5.py
@@ -1,7 +1,21 @@
-from random import shuffle, choice
+import random
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-shuffle(numbers)
-print(numbers)
-random_number = choice(numbers)
-print(random_number)
+
+def random_permutation_with_first_five(n):
+    if n < 5:
+        raise ValueError("N должно быть больше или равно 5")
+
+    array = list(range(1, n + 1))
+    array.remove(5)  # Удаляем 5 из списка
+    random.shuffle(array)  # Перемешиваем оставшиеся элементы
+    array.insert(0, 5)  # Вставляем 5 на первое место
+    return array
+
+
+# Ввод данных
+n = int(input("Введите N (N >= 5): "))
+
+
+# Заполнение массива
+result = random_permutation_with_first_five(n)
+print("Случайная перестановка с первым элементом равным 5:", result)

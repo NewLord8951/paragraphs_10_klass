2c8a6c3b88e8eb1d371c36dc766b9f43f4ebb3ed
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:23:08 +0000

    62.7

--- task62.7.py
+++ task62.7.py
@@ -1,9 +1,12 @@
 from random import randint
 
-lst = [randint(1000, 2000) for i in range(10)]
-count = 0
-for number in lst:
-    if ((number % 100) // 10) % 2 == 0:
-        count += 1
-print(lst)
-print(count)
+a = []
+for i in range(100): 
+    a.append(randint(1000, 2000))
+print(f"2)\n{a}\n")
+for i in range(1, len(a), 2):
+    first = a[i-1]
+    second = a[i]
+    a[i-1] = second
+    a[i] = first
+print(a)

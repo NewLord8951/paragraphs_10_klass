577f259f251e4f87c5d56515c52d88bb864264a7
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:30:02 +0000

    63.3

--- task63.3.py
+++ task63.3.py
@@ -1,17 +1,11 @@
-a = input().split()
-min1 = float(a)[0]
-min2 = float(a)[0]
-min3 = float(a)[0]
-for i in range(len(a)):
-    if float(a[i]) < min1:
-        min3 = min2
-        min2 = min1
-        min1 = float(a[i])
-    elif float(a[i]) < min2:
-        min3 = min2
-        min2 = float(a[i])
-    elif float(a[i]) < min3:
-        min3 = float(a[i])
-print(min1)
-print(min2)
-print(min3)
+def find_three_minimus(arr):
+    unique_elements = list(set(arr))
+    unique_elements.sort()
+    if len(unique_elements) < 3:
+        print("Недостаточно уникальных элементов.")
+        return
+    return unique_elements[:3]
+
+
+array = [7, 1, 3, 2, 5, 2, 4]
+print("Три минимума:", find_three_minimus(array))

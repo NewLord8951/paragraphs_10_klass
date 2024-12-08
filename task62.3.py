900a1a9bc94729541e5e2abad6efccb20b4509a4
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:21:51 +0000

    62.3

--- task62.3.py
+++ task62.3.py
@@ -1,7 +1,13 @@
-def fibonacci_function(n):
-    result = []
-    a, b = 0, 1
-    for _ in range(n):
-        result.append(a)
-        a, b = b, a + b
-    return result
+n = int(input("Введите размер массива: "))
+
+
+def fibonacci(n):
+    fib = [0, 1]
+    for x in range(2, n):
+        fib.append(fib[-2] + fib[-1])
+
+    return fib[:n]
+
+
+result = fibonacci(n)
+print("Числа Фибоначчи:", result)

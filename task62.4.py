5b242f554de7e579062c46109b96bb3806682c47
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:22:11 +0000

    62.4

--- task62.4.py
+++ task62.4.py
@@ -1,3 +1,6 @@
-N = 10
-array = [i + 1 for i in range(N)]
-print(array)
+from random import randint
+
+n = int(input("Введите количество элементов: "))
+a = [randint(0, 100) for i in range(1, n+1)]
+
+print(f'Ваш начальное количество элементов было: {n}, вот список: {a}')

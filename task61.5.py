6cadec1de534f9a6afd5bd0937f203d768d90d8b
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:13:19 +0000

    61.5

--- task61.5.py
+++ task61.5.py
@@ -1,2 +1,10 @@
-n = input('Введи двоичное число: ')
-print(int(n, base=2))
+def bin_dec(N, k=0, R=0):
+    ts = N % 10
+    R += ts * 2 ** k
+    if N > 1:
+        return bin_dec(N // 10, k + 1, R)
+    return R
+
+
+number = int(input("напиши число из 10 счисление "))
+print(f'число {number} переводим в десятичную счисление: {bin_dec(number)}')

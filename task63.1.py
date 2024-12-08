bfea9e8991526a4aa3000aeefdee13662b3c35be
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-08 09:29:25 +0000

    63.1

--- task63.1.py
+++ task63.1.py
@@ -1,4 +1,4 @@
-mas = [int(i) for i in input('Введите массив : ').split()]
+mas = [int(i) for i in input("Введите массив: ").split()]
 flag = 0
 for i in mas:
     if i % 2 == 0:
@@ -6,11 +6,12 @@ for i in mas:
         flag = 1
         break
 if flag == 0:
-    print('Четных нет!')
+    print("Четных нет")
     exit()
 for i in mas:
     if i % 2 == 0 and i > max:
         max = i
     if i % 2 == 0 and i < min:
         min = i
+
 print(max, min)

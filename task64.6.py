2fdac4972325e9219d9be99617df0ab8a1df8364
NewLord8951 <alexandrmaniraki@gmail.com>
2024-12-20 07:57:53 +0000

    64.6

--- task64.6.py
+++ task64.6.py
@@ -1,33 +1,12 @@
-import random
+from random import randint
 
-n = int(input("Введите количество элементов в массиве: "))
 
-arr = [random.randint(1, 100) for i in range(n)]
-print("Исходный массив: ", arr)
+def swap_adjacent(arr):
+    for i in range(0, len(arr) - 1, 2):
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    return arr
 
-if n % 2 != 0:
-    mid = arr[n // 2]
-    left_arr = arr[:n // 2]
-    right_arr = arr[n // 2 + 1:]
-else:
-    left_arr = arr[:n // 2]
-    right_arr = arr[n // 2:]
 
-for i in range(len(left_arr)):
-    for j in range(len(left_arr) - 1 - i):
-        if left_arr[j] > left_arr[j + 1]:
-            left_arr[j], left_arr[j + 1] = left_arr[j + 1], left_arr[j]
-
-for i in range(len(right_arr)):
-    min_idx = i
-    for j in range(i + 1, len(right_arr)):
-        if right_arr[j] > right_arr[min_idx]:
-            min_idx = j
-        right_arr[i], right_arr[min_idx] = right_arr[min_idx], right_arr[i]
-
-if n % 2 != 0:
-    arr = left_arr + [mid] + right_arr
-else:
-    arr = left_arr + right_arr
-
-print("Отсортированный массив: ", arr)
+array = [randint(0, 10) for _ in range(10)]
+print("Исходный массив: ", array)
+print("Массив после перестановки: ", swap_adjacent(array))
